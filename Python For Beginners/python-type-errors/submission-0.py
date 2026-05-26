5a5376b90960320bbf7037e4bfa9010abbb8032e
Oumer Gharad <ofgharad@gmail.com>
2026-05-26 16:27:33 -0400

    Add: python-type-errors - submission-0

---
+++ Python For Beginners/python-type-errors/submission-0.py
@@ -0,0 +1,14 @@
+
+
+variable = 10
+variable = str(variable)
+
+
+# Don't modify the code below this line
+print(variable)
+print(type(variable))
+
+variable = int(variable)
+
+print(variable)
+print(type(variable))
